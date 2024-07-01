d9ff789337eb4261a7f3fcb2c3178c9bc65237d4
Herlea Marius <mariusherlea@gmail.com>
2024-07-01 12:58:41 +0300

    Services

--- components/Services.jsx
+++ components/Services.jsx
@@ -31,7 +31,28 @@ const Services = () => {
           My Services
         </h2>
         {/** grid items*/}
-        <div>grid item</div>
+        <div className="grid xl:grid-cols-3 justify-center gap-y-12 xl:gap-y-24 xl:gap-x-8">
+          {servicesData.map((item, index) => {
+            return (
+              <Card
+                className="w-full max-w-[424px] h-[300px] flex flex-col pt-16 pb-10 justify-center items-center relative"
+                key={index}
+              >
+                <CardHeader className="text-primary absolute -top-[60px]">
+                  <div className="w-[140px] h-[80px] flex bg-white dark:bg-background justify-center items-center">
+                    {item.icon}
+                  </div>
+                </CardHeader>
+                <CardContent className="text-center">
+                  <CardTitle className="mb-4">{item.title}</CardTitle>
+                  <CardDescription className="text-lg">
+                    {item.description}
+                  </CardDescription>
+                </CardContent>
+              </Card>
+            );
+          })}
+        </div>
       </div>
     </section>
   );
