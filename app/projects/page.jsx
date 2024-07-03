58e8adbd0a998ec85c57045230a989f2468e3d68
Herlea Marius <mariusherlea@gmail.com>
2024-07-03 09:55:03 +0300

    projects/page.jsx

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,7 +1,122 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { Tabs, TabsList, TabsContent, TabsTrigger } from "@/components/ui/tabs";
+import ProjectCard from "@/components/ProjectCard";
+
+const projectData = [
+  {
+    image: "/work/4.png",
+    category: "react js",
+    name: "Ceva Website",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/3.png",
+    category: "next js",
+    name: "Altcdeva Website",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.  ",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/2.png",
+    category: "vanilla js",
+    name: "Si mai Website",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/1.png",
+    category: "html css",
+    name: "Vai viata lui Website",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/4.png",
+    category: "react js",
+    name: "Ceva Website",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/3.png",
+    category: "next js",
+    name: "Altcdeva Website",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/2.png",
+    category: "vanilla js",
+    name: "Si mai Website",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/1.png",
+    category: "html css",
+    name: "Vai viata lui Website",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+    link: "/",
+    github: "/",
+  },
+];
+
+// remove category duplicates
+const uniqueCategories = [
+  "all projects",
+  ...new Set(projectData.map((item) => item.category)),
+];
 
 const Projects = () => {
-  return <div>Projects Page</div>;
+  const [categories, setCategories] = useState(uniqueCategories);
+  const [activeCategory, setActiveCategory] = useState("all projects");
+
+  const filteredProjects = projectData.filter(
+    (project) =>
+      activeCategory === "all projects" || project.category === activeCategory
+  );
+
+  return (
+    <section className="min-h-screen pt-12">
+      <div className="container mx-auto">
+        <h2 className="section-title mb-8 xl:mb-16 text-center mx-auto">
+          My projects
+        </h2>
+        {/** tabs*/}
+        <Tabs defaultValue={activeCategory} className="mb-24 xl:mb-48">
+          <TabsList className="w-full grid h-full md:grid-cols-4 lg:max-w-[640px] mb-12 mx-auto md:border dark:border-none">
+            {categories.map((category, index) => (
+              <TabsTrigger
+                onClick={() => setActiveCategory(category)}
+                key={index}
+                value={category}
+                className="capitalize w-[162px] md:w-auto"
+              >
+                {category}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+          {/** tabs content*/}
+          <div className="text-lg xl:mt-8 grid grid-cols-1 lg:grid-cols-3 gap-4">
+            {filteredProjects.map((project, index) => (
+              <TabsContent key={index} value={activeCategory}>
+                <ProjectCard project={project} />
+              </TabsContent>
+            ))}
+          </div>
+        </Tabs>
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
