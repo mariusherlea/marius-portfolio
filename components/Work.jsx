55afabed133b8cdfc309f8ff8a059940ed6b85a8
Herlea Marius <mariusherlea@gmail.com>
2024-07-05 12:22:18 +0300

    latest project descrition

--- components/Work.jsx
+++ components/Work.jsx
@@ -30,7 +30,7 @@ const Work = () => {
         <div className="max-w-[400px] mx-auto xl:mx-0 text-center xl:text-left mb-12 xl:h-[400px] flex flex-col justify-center items-center xl:items-start">
           <h2 className="section-title mb-4">Latest Projects</h2>
           <p className="subtitle mb-8">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Proud to showcase some of my latest projects.
           </p>
           <Link href="/projects">
             <Button>All projects</Button>
