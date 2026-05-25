8baa8d3db4af8e9647ca302cec42c985a8e12cc6
marius herlea <mariusherlea@gmail.com>
2026-05-25 10:05:47 +0200

    Update sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -3,6 +3,22 @@ export default function sitemap() {
     {
       url: "https://marius-portfolio-kappa.vercel.app",
       lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+
+    {
+      url: "https://marius-portfolio-kappa.vercel.app/projects",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+
+    {
+      url: "https://marius-portfolio-kappa.vercel.app/contact",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
     },
   ];
 }
