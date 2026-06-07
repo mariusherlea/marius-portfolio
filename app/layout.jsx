96a5c51e8170a725318de9b609ff320cb4538083
marius herlea <mariusherlea@gmail.com>
2026-06-07 12:04:42 +0200

    Fix layout and update custom domain SEO

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,14 @@
+//app/layout.jsx
+import { Outfit } from "next/font/google";
+import "./globals.css";
+
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import { ThemeProvider } from "@/components/ThemeProvider";
+
+const outfit = Outfit({
+  subsets: ["latin"],
+});
 export const metadata = {
   metadataBase: new URL("https://www.mariusherlea.dev"),
 
@@ -67,3 +78,16 @@ export const metadata = {
     follow: true,
   },
 };
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={outfit.className}>
+        <ThemeProvider attribute="class" defaultTheme="light">
+          <Header />
+          {children}
+          <Footer />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
